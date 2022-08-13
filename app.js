2c335881f3ac22b7fd62137441ba9309013e8a50
Prateek Chaurasia <prateek18801@gmail.com>
2022-08-13 16:55:35 +0530

    add cluster scaling

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-if (process.env.NODE_ENV !== 'prod') require('dotenv').config();
+if (process.env.NODE_ENV === 'dev') require('dotenv').config();
+
+const cluster = require('node:cluster');
+const os = require('node:os');
 const express = require('express');
 
 const app = express();
@@ -6,10 +9,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/', (req, res) => {
-    res.send('server running');
-});
 
-app.listen(process.env.PORT, process.env.HOST, () => {
-    console.log(`server running on ${process.env.HOST}:${process.env.PORT}`);
-});
+if (cluster.isPrimary) {
+    console.log(`master ${process.pid} starting cluster`);
+
+    for (let i = 0; i < os.cpus().length; i++) { cluster.fork(); }
+
+    cluster.on('exit', (worker, code, signal) => {
+        if (signal) {
+            console.log(`worker ${worker.process.pid} killed signal: ${signal}`);
+        } else if (code !== 0) {
+            console.log(`worker ${worker.process.pid} exited code: ${code}`);
+        } else {
+            console.log(`worker ${worker.process.pid} died`);
+        }
+        cluster.fork();
+    });
+} else {
+    app.listen(process.env.PORT, process.env.HOST, () => {
+        console.log(`worker running on ${process.env.HOST}:${process.env.PORT} - pid: ${process.pid}`);
+    });
+}
