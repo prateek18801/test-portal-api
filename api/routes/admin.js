e254abcdf405e49ef03fa52e71c02aa77b9cb33d
Prateek Chaurasia <prateek18801@gmail.com>
2022-08-15 00:32:37 +0530

    add question routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,20 +2,18 @@ const router = require('express').Router();
 
 const adminController = require('../controllers/admin');
 
-// question routes
+// test routes
 router.get('/test/:id?', adminController.getTest);
 
 router.post('/test/:id?', adminController.postTest);
 
 router.delete('/test/:id', adminController.deleteTest);
 
-// // test routes
-// router.get('/question/:id');
+// question routes
+router.get('/question/:id', adminController.getQuestion);
 
-// router.post('/question/:testId');
+router.post('/question/:id?', adminController.postQuestion);
 
-// router.patch('/question/:id');
-
-// router.delete('/question/:id');
+router.delete('/question/:id', adminController.deleteQuestion);
 
 module.exports = router;
