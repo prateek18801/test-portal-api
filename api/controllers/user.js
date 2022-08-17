c251ab294c56ec1f2a97d604d8cbf3013a76d009
Prateek Chaurasia <prateek18801@gmail.com>
2022-08-18 03:35:24 +0530

    add post response and delete response

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -32,3 +32,71 @@ exports.getTest = async (req, res, next) => {
     }
 }
 
+exports.postResponse = async (req, res, next) => {
+    const _id = req.params.id;
+
+    const data = {
+        ...req.body,
+        timestamp: Date.now()
+    };
+
+    try {
+        const response = await Response.findById(_id);
+
+        if (!response) {
+            const error = new Error('not registered');
+            error.code = 400;
+            throw error;
+        }
+
+        response.answers = updateList(response.answers, data);
+
+        const saved = await response.save();
+        return res.status(200).json({
+            message: 'saved',
+            data: saved
+        });
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.deleteResponse = async (req, res, next) => {
+    const _id = req.params.id;
+    const q_id = req.query.q;
+    try {
+
+        const response = await Response.findById(_id);
+        response.answers = removeElement(response.answers, q_id);
+
+        const saved = await response.save();
+        return res.status(200).json({
+            message: 'deleted',
+            data: saved
+        });
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+// utility functions
+
+function updateList(list, object) {
+    const idx = list.findIndex(element => element.question.toString() === object.question);
+    if (idx > -1) {
+        list[idx] = object;
+        return list;
+    }
+    return [...list, object];
+}
+
+function removeElement(list, id) {
+    const idx = list.findIndex(element => element.question.toString() === id);
+    if (idx > -1) {
+        list.splice(idx, 1);
+    }
+    return list;
+}
