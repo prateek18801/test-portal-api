dde987a19f467ae0da51a57a26cf7bed4cee0b0f
Prateek Chaurasia <prateek18801@gmail.com>
2022-08-19 12:40:47 +0530

    add results routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -19,4 +19,6 @@ router.delete('/question/:id', adminController.deleteQuestion);
 // evaluation routes
 router.get('/evaluate/:id', adminController.evaluateTest);
 
+router.get('/results/:id', adminController.getResults);
+
 module.exports = router;
