50e1119f63663ab505b710389bc1dfaefebb2469
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-10 20:32:03 +0530

    remove cluster module

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV === 'dev') require('dotenv').config();
-
-const cluster = require('node:cluster');
-const os = require('node:os');
 const express = require('express');
 
 require('./api/utils/db').connect();
@@ -15,27 +11,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api', userRoutes);
-app.use('/api/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(errorHandler);
 
-if (cluster.isPrimary) {
-    console.log(`master ${process.pid} starting cluster`);
-
-    for (let i = 0; i < os.cpus().length; i++) { cluster.fork(); }
-
-    cluster.on('exit', (worker, code, signal) => {
-        if (signal) {
-            console.log(`worker ${worker.process.pid} killed signal: ${signal}`);
-        } else if (code !== 0) {
-            console.log(`worker ${worker.process.pid} exited code: ${code}`);
-        } else {
-            console.log(`worker ${worker.process.pid} died`);
-        }
-        cluster.fork();
-    });
-} else {
-    app.listen(process.env.PORT, process.env.HOST, () => {
-        console.log(`worker running on ${process.env.HOST}:${process.env.PORT} - pid: ${process.pid}`);
-    });
-}
+app.listen(process.env.PORT, () => {
+    console.log(`worker running on PORT: ${process.env.PORT}`);
+});
